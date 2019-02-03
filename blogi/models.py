5bf3a294dbaf77d5795bdaca3f8400f6e46e923a
Hal Vong <halvong@yahoo.com>
2019-02-03 11:52:53 -0800

    add templates

--- blogi/models.py
+++ blogi/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -30,6 +31,13 @@ class Post(models.Model):
 
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
+
+    def get_absolute_url(self):
+        return reverse('blogi:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
     class Meta:
         ordering = ('-publish',)
